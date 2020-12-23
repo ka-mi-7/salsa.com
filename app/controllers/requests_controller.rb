3944eaab9a12946c940690c312844c653f0c75f3
masayuki <kik557473@gmail.com>
2020-12-23 15:44:21 +0000

    requests controller 作成

---
+++ app/controllers/requests_controller.rb
@@ -0,0 +1,21 @@
+class RequsetsController < ApplicationController
+  def create
+    current_team.requests.create!(recruit_id: params[:recruit_id])
+    
+    redirect_to recruit_path(params[:recruit_id])
+  end
+
+  def approve
+    recruit = Recruit.find(params[:recruit_id])
+    request = recruit.requests.find(params[:id])
+    request.approval!
+    redirect_to recruit_path(params[:recruit_id])
+  end
+  
+  def disapprove
+    request = Recruit.find(params[:recruit_id]).requests.find(params[:id])
+    request.disapproval!
+    redirect_to recruit_path(params[:recruit_id])
+  end
+end
+
--- app/controllers/requsets_controller.rb
+++
@@ -1,2 +0,0 @@
-class RequsetsController < ApplicationController
-end
